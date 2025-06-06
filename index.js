5ecaa7fad6b530b89dcb81d42ae1aa5db8ad0d82
peter munyambu <petermunyambu1000@gmail.com>
2025-06-06 03:19:28 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression: divides 2000 by 100
+const divide = function() {
+  return 2000 / 100;
+}
+
+// Arrow function: squares one parameter
+const square = (x) => x * x;
+
+// Arrow function: adds two parameters
+const add = (a, b) => a + b;
